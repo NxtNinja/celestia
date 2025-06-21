057cee8dd1c75f1a867b93dd7f73d2e8f6244fa3
PRIYANGSU BANIK <136341993+NxtNinja@users.noreply.github.com>
2025-06-22 01:19:48 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="text-sm  text-gray-500 text-center p-6 border-t border-gray-800 mt-10">
-      © 2025 SkyScope — Satellite Tracking with N2YO API + Mapbox
+      © 2025 SkyScope — Satellite Tracking with N2YO API + Leaflet.js
     </footer>
   );
 };
