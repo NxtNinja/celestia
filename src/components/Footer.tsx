9f195a6b1a4b5c389fb0aebf046f377f81b092e8
PRIYANGSU BANIK <136341993+NxtNinja@users.noreply.github.com>
2025-06-22 12:00:08 +0530

    SEO fixes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,22 @@
+import Link from "next/link";
+
 const Footer = () => {
   return (
     <footer className="text-sm  text-gray-500 text-center p-6 border-t border-gray-800 mt-10">
-      © 2025 SkyScope — Satellite Tracking with N2YO API + Leaflet.js
+      <p className="">
+        © {new Date().getFullYear()} Celvo — Satellite Tracking with N2YO API +
+        Leaflet.js
+      </p>
+      <p className="">
+        Made with ❤️ by{" "}
+        <Link
+          className="text-blue-600"
+          href={"https://priyangsubanik.in/"}
+          target="_blank"
+        >
+          Priyangsu Banik
+        </Link>
+      </p>
     </footer>
   );
 };
